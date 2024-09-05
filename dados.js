4d2790d6989aaddb7dd170984ad65bee2ac9157a
Guerreiro <marcosguerreiro@hotmail.com>
2024-09-04 22:34:22 -0300

    Primeiro commit

---
+++ dados.js
@@ -0,0 +1,144 @@
+
+let dados = [
+    {
+        marca: 'Fiat',
+        modelo: 'Uno',
+        placa: 'ABC-1234',
+        quilometragem: 100000,
+        oleo: 'Shell 15W40',
+      },
+      {
+        marca: 'Fiat',
+        modelo: 'Mobi',
+        placa: 'DEF-5678',
+        quilometragem: 50000,
+        oleo:'Mobil 5w30',
+      },
+      {
+        marca: 'Fiat',
+        modelo: 'Argo',
+        placa: 'GHI-9012',
+        quilometragem: 20000,
+        oleo:'Mobil 5w30'
+      },
+    
+      {
+        marca: 'Volkswagen',
+        modelo: 'Gol',
+        placa: 'JKL-3456',
+        quilometragem: 80000,
+        oleo: 'Shell 15W40'
+      },
+      {
+        marca: 'Volkswagen',
+        modelo: 'Voyage',
+        placa: 'MNO-7890',
+        quilometragem: 40000,
+        oleo: 'Shell 15W40'
+      },
+      {
+        marca: 'Volkswagen',
+        modelo: 'Nivus',
+        placa: 'PQR-1234',
+        quilometragem: 15000,
+        oleo: 'Mobil 0W20'
+      },
+    
+      {
+        marca: 'Chevrolet',
+        modelo: 'Onix',
+        placa: 'STU-5678',
+        quilometragem: 70000,
+        oleo: 'Mobil 0W20'
+      },
+      {
+        marca: 'Chevrolet',
+        modelo: 'Cruze',
+        placa: 'VWX-9012',
+        quilometragem: 60000,
+        oleo: 'Shell 5W30'
+      },
+      {
+        marca: 'Chevrolet',
+        modelo: 'Trailblazer',
+        placa: 'YZX-3456',
+        quilometragem: 30000,
+        oleo: 'Mobil 5W30'
+      },
+      {marca: 'Fiat', 
+       modelo: 'Cronos', 
+       placa: 'ABC-4321', 
+       quilometragem: 45000,
+       oleo: 'Shell 5w30'  
+      },
+      { marca: 'Fiat', 
+        modelo: 'Fastback', 
+        placa: 'DEF-4321', 
+        quilometragem: 22000,
+        oleo: 'Mobil 0W20' 
+      },
+      { marca: 'Fiat', 
+        modelo: 'Toro', 
+        placa: 'GHI-4321', 
+        quilometragem: 68000,
+        oleo: 'mobil 10w30', 
+      },
+      { marca: 'Fiat', 
+        modelo: 'Strada', 
+        placa: 'JKL-4321', 
+        quilometragem: 31000,
+        oleo: 'Shell 0W20' 
+      },
+      { marca: 'Fiat', 
+        modelo: 'Idea',
+        placa: 'MNO-4321', 
+        quilometragem: 90000,
+        oleo: 'Mobil 15W40' 
+      },
+      { marca: 'Fiat', 
+        modelo: 'Grand Siena', 
+        placa: 'PQR-4321', 
+        quilometragem: 55000,
+        oleo: 'Shell 5W30', 
+       },
+      { marca: 'Fiat', 
+        modelo: 'Doblò', 
+        placa: 'STU-4321', 
+        quilometragem: 82000,
+        oleo: 'Shell 10W40', 
+       },
+      { marca: 'Fiat', 
+        modelo: 'Fiorino', 
+        placa: 'VWX-4321', 
+        quilometragem: 11000,
+        oleo: 'Mobil 15W40' 
+       },
+      { marca: 'Fiat', 
+        modelo: 'Pulse', 
+        placa: 'YZX-4321', 
+        quilometragem: 7000,
+        oleo: 'Shell 0W20', 
+       },
+      { marca: 'Fiat', 
+        modelo: 'Mobi Trekking', 
+        placa: 'AAA-4321', 
+        quilometragem: 15000,
+        oleo: 'Mobil 0w20' 
+       },
+        
+      { marca: 'Volkswagen', 
+        modelo: 'Virtus', 
+        placa: 'ABC-5432',
+        quilometragem: 38.000,
+        oleo: 'Shell 5W20'
+       },
+
+       {
+        marca: 'Chevrolet',
+        modelo: 'Cobalt LTZ',
+        placa: 'GEG3720',
+        quilometragem: 123.500,
+        oleo: 'Mobil 0W20',
+       }
+];  
+
